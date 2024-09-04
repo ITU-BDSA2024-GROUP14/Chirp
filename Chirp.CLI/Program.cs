9b63a43e0face22693e671bbacf045e5e24d6059
Daniel <dtha@itu.dk>
2024-09-04 16:06:19 +0200

    making csv wrtiting easier using external library
    Co-authored-by: August <aubu@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System.Globalization;
 using System.Text.RegularExpressions;
+using CsvHelper;
 
 class Program
 {
-    private static string _path = "/home/ostarup/ITU/BDSA/Chirp/Chirp.CLI/chirp_cli_db.csv";
+    public record Cheep(string Author, string Message, long Timestamp);
+    private static string DATABASE_PATH = "./chirp_cli_db.csv";
 
+    private static CultureInfo CULTURE_INFO = new CultureInfo("en-DE");
     //Regex and date time information found here:
     //https://learn.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.regex.-ctor?view=net-7.0#system-text-regularexpressions-regex-ctor(system-string)
     //https://learn.microsoft.com/en-us/dotnet/api/system.datetimeoffset.utcnow?view=net-7.0
     public static void PrintCheeps()
     {
-        var lines = File.ReadAllLines(_path);
+        var lines = File.ReadAllLines(DATABASE_PATH);
         foreach (var line in lines.Skip(1))
         {
             var regex = new Regex(@"(?<!"".*),|,(?!.*"")");
@@ -20,7 +23,7 @@ class Program
             var timestamp = DateTimeOffset.FromUnixTimeSeconds(long.Parse(items[2]));
             //The following lines takes the date time and converts it to local time, and then prints it in the desired format
             var time = timestamp.DateTime.ToLocalTime();
-            var cheep = author + " @ " + time.ToString("dd/MM/yy HH:mm:ss", new CultureInfo("en-DE")) + ": " + message + "\n";
+            var cheep = author + " @ " + time.ToString("dd/MM/yy HH:mm:ss", CULTURE_INFO)+ ": " + message + "\n";
             Console.Write(cheep);
         }
     }
@@ -30,12 +33,14 @@ class Program
         var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
         var author = Environment.UserName;
 
-        var cheep = author + ",\"" + message + "\"," + timestamp;
+        var cheep = new Cheep(author, message, timestamp);
 
-        using var writer = File.AppendText(_path);
-
-        writer.WriteLine(cheep);
-
+        using (var stream = File.Open(DATABASE_PATH, FileMode.Append))
+        using (var writer = new StreamWriter(stream))
+        using (var csv = new CsvWriter(writer, CULTURE_INFO))
+        {
+            csv.WriteRecord(cheep);
+        }
     }
 
     public static void Main(string[] args)
