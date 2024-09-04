b8e0f7d5bb200fe768b26e4076b971f4b25a14bc
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-09-04 18:19:33 +0200

    Use top-level-statements in order to reduce boilerplate code

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,58 +1,46 @@
-﻿using System.Globalization;
-using System.Text.RegularExpressions;
+﻿using Chirp.CLI;
 
-using Chirp.CLI;
-
-class Program
+if (args.Length < 1)
 {
-    private static string _path = "./chirp_cli_db.csv";
+    UserInterface.PrintError("Must have at least one argument");
+    return;
+}
 
-    //Regex and date time information found here:
-    //https://learn.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.regex.-ctor?view=net-7.0#system-text-regularexpressions-regex-ctor(system-string)
-    //https://learn.microsoft.com/en-us/dotnet/api/system.datetimeoffset.utcnow?view=net-7.0
-    public static void PrintCheeps()
-    {
-        var lines = File.ReadAllLines(_path);
-        foreach (var line in lines.Skip(1))
+switch (args[0])
+{
+    case "read":
+        PrintCheeps();
+        break;
+    case "cheep":
+        if (args.Length < 2)
         {
-            UserInterface.PrintCheep(line);
-        }
-    }
-
-    private static void CreateCheep(string message)
-    {
-        var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-        var author = Environment.UserName;
-
-        var cheep = author + ",\"" + message + "\"," + timestamp;
-
-        using var writer = File.AppendText(_path);
-
-        writer.WriteLine(cheep);
-
-    }
-
-    public static void Main(string[] args)
-    {
-        if (args.Length < 1)
-        {
-            UserInterface.PrintError("Must have at least one argument");
+            UserInterface.PrintError("Needs more arguments");
             return;
         }
 
-        switch (args[0])
-        {
-            case "read":
-                PrintCheeps();
-                break;
-            case "cheep":
-                if (args.Length < 2)
-                {
-                    UserInterface.PrintError("Needs more arguments");
-                    return;
-                }
-                CreateCheep(args[1]);
-                break;
-        }
+        CreateCheep(args[1]);
+        break;
+}
+
+const string path = "./chirp_cli_db.csv";
+
+static void PrintCheeps()
+{
+    var lines = File.ReadAllLines(path);
+    foreach (var line in lines.Skip(1))
+    {
+        UserInterface.PrintCheep(line);
     }
+}
+
+static void CreateCheep(string message)
+{
+    var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    var author = Environment.UserName;
+
+    var cheep = author + ",\"" + message + "\"," + timestamp;
+
+    using var writer = File.AppendText(path);
+
+    writer.WriteLine(cheep);
 }
