3a572b2b8b1e8a0231c4a0a92ecd272dfa0f933d
osta <osta@itu.dk>
2024-09-09 09:12:48 +0200

    Added docopt usage to parse args
    Co-authored-by: jonv <jonv@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -3,9 +3,24 @@ using System.Text.RegularExpressions;
 
 using Chirp.CLI;
 
+using DocoptNet;
+
 class Program
 {
     private static string _path = "./chirp_cli_db.csv";
+
+    //Using @ to create verbatim string, which means that no escapes are needed
+    //https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/verbatim
+    private const string Usage = @"Chirp.
+
+Usage:
+  chirp read
+  chirp cheep <message>
+  chirp (-h | --help)
+
+Options:
+  -h --help  Show this screen.
+";
 
     //Regex and date time information found here:
     //https://learn.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.regex.-ctor?view=net-7.0#system-text-regularexpressions-regex-ctor(system-string)
@@ -34,6 +49,8 @@ class Program
 
     public static void Main(string[] args)
     {
+        var arguments = new Docopt().Apply(Usage, args);
+        Console.WriteLine(arguments);
         if (args.Length < 1)
         {
             UserInterface.PrintError("Must have at least one argument");
