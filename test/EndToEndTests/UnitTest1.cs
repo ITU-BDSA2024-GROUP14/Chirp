d24632d3af4d8b42181ef2443e352859010f050a
Daniel <dtha@itu.dk>
2024-09-11 14:33:13 +0200

    create test directory with test projects

---
+++ test/EndToEndTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EndToEndTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/SimpleDB.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SimpleDB.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
