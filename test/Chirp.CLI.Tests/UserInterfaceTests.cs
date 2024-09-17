62eab132e422ef6375d1b373017d01d10a813c3e
Esser <jones@itu.dk>
2024-09-17 13:54:47 +0200

    Unit test for PrintCheeps

--- test/Chirp.CLI.Tests/UserInterfaceTests.cs
+++ test/Chirp.CLI.Tests/UserInterfaceTests.cs
@@ -20,4 +20,26 @@ public class UserInterfaceTests
             Assert.Contains("05/11/55 06:00:00", result);
         }
     }
+    
+    [Fact]
+    public void PrintCheepsContainsContent()
+    {
+        using (StringWriter sw = new())
+        {
+            Console.SetOut(sw);
+
+            Cheep[] cheeps = new Cheep[]
+            {
+                new("Eva", "Jeg elsker æbler", -46752800),
+                new("John Doe", "I'm 6', dont bother me" , 72350352)
+            };
+            UserInterface.PrintCheeps(cheeps);
+
+            string result = sw.ToString();
+            Assert.Contains("Eva", result);
+            Assert.Contains("Jeg elsker æbler", result);
+            Assert.Contains("John Doe", result);
+            Assert.Contains("I'm 6', dont bother me", result);
+        }
+    }
 }
