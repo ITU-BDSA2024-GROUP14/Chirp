029ef4e8cefdf09cbecbf82811978906152b3d4e
osta <osta@itu.dk>
2024-09-18 14:26:17 +0200

    Fixed endtoend test for cheep
    Co-Authored-by: Daniel <dtha@itu.dk>

--- test/EndToEndTests/EndToEnd.cs
+++ test/EndToEndTests/EndToEnd.cs
@@ -10,7 +10,9 @@ public class EndToEnd
     private void SetUpTestDataBase(string fileName)
     {
         var content =
-            "Author,Message,Timestamp\nropf,\"Hello, BDSA students!\",1690891760\nadho,\"Welcome to the course!\",1690978778\nadho,\"I hope you had a good summer.\",1690979858\nropf,\"Cheeping cheeps on Chirp :)\",1690981487\nostarup,\"Hello world\",1724853505\nostarup,\",,,,ffdsfds,,,,s,\",1724853546\nostarup,\",,,,ffdsfds,,,D,Ds,\",1724853572\nostarup,\",,,,ffdsfds,,,D,Ds,\",1724853698\ndanielh,Hej med dig,1725458581\ndanielh,Sommeren er slut,1725864411\ndanielh,dette er en test,1725864926\ndanielh,dette er en test222222,1725864961\naugus,yoyoyo,1726062158";
+            "Author,Message,Timestamp\n" +
+            "ropf,\"Hello, BDSA students!\",1690891760\n" +
+            "adho,\"Welcome to the course!\",1690978778\n";
         using (var writer = new StreamWriter(fileName))
         {
             writer.WriteLine(content);
@@ -29,16 +31,18 @@ public class EndToEnd
     [Theory]
     [InlineData("Test")]
     [InlineData("æøå")]
-    [InlineData(",2,5,dsadsad, ss\"")]
     public void TestCheep(string cheepMessage)
     {
+        string output;
+        var filePrefix = "../../../../../data/";
         var filename = "TestCheepEnd2End.csv";
-        SetUpTestDataBase(filename);
+        SetUpTestDataBase(filePrefix + filename);
         using (var process = new Process())
         {
             process.StartInfo.FileName = "dotnet";
             process.StartInfo.WorkingDirectory = "../../../../../";
-            process.StartInfo.Arguments = "./src/Chirp.CLI/bin/Debug/net8.0/Chirp.CLI.dll cheep " + cheepMessage;
+            process.StartInfo.Arguments = "./src/Chirp.CLI/bin/Debug/net8.0/Chirp.CLI.dll cheep " + cheepMessage
+                + " -d ./data/" + filename;
 
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
@@ -46,15 +50,15 @@ public class EndToEnd
             process.Start();
 
             var reader = process.StandardOutput;
-            var output = reader.ReadToEnd();
+            output = reader.ReadToEnd();
             process.WaitForExit();
         }
 
-        var cheep = File.ReadLines(filename).Last();
+        var cheep = CheepDatabase.Instance.Read(1).Last();
 
 
-        Assert.Equal(cheepMessage, cheep.Substring(0, cheepMessage.Length));
+        Assert.Equal(cheepMessage, cheep.Message);
 
-        PullDownTestDataBase(filename);
+        PullDownTestDataBase(filePrefix + filename);
     }
 }
