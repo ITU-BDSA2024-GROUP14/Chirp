83c328d8e622083d5373851e23769578685d52c9
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-09-20 18:08:20 +0200

    Added limit path parameter option in GET /cheeps endpoint
    Use like http://localhost:5016/cheeps?limit=3

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -6,6 +6,6 @@ var app = builder.Build();
 var database = CheepDatabase.Instance;
 
 app.MapPost("/cheep", (Cheep cheep) => database.Store(cheep));
-app.MapGet("/cheeps", () => database.Read());
+app.MapGet("/cheeps", (int? limit = null) => database.Read(limit));
 
 app.Run();
