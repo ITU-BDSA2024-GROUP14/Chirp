704491ad55274c9c5254fd803b527c9ed1c690e2
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-09-23 09:27:12 +0200

    Use test db path when in development env
    Co-authored-by: Jonas Esser <jones@itu.dk>

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -5,6 +5,11 @@ var app = builder.Build();
 
 var database = CheepDatabase.Instance;
 
+if (app.Environment.IsDevelopment())
+{
+    database.ChangeCsvPath("../../data/chirp_cli_dev_db.csv");
+}
+    
 app.MapPost("/cheep", (Cheep cheep) => database.Store(cheep));
 app.MapGet("/cheeps", (int? limit = null) => database.Read(limit));
 
