9c047c17bf38d7b6c2490136fb734efd9a419e34
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-09-24 13:35:00 +0200

    Validate cheep author and message is not null

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,11 +1,27 @@
 using Chirp.CSVDBService;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var database = CheepDatabase.Instance;
 
-app.MapPost("/cheep", (Cheep cheep) => database.Store(cheep));
+app.MapPost("/cheep", Results<BadRequest<string>, Ok> (Cheep cheep) =>
+{
+    if (string.IsNullOrWhiteSpace(cheep.Author))
+    {
+        return TypedResults.BadRequest("Author is required");
+    }
+
+    if (string.IsNullOrWhiteSpace(cheep.Message))
+    {
+        return TypedResults.BadRequest("Message is required");
+    }
+
+    database.Store(cheep);
+
+    return TypedResults.Ok();
+});
 app.MapGet("/cheeps", (int? limit = null) => database.Read(limit));
 
 app.Run();
