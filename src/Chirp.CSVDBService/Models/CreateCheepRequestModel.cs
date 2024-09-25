76f26ec4336f96b533ede01b08ab8ef31fc8ec53
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-09-25 15:22:12 +0200

    Make properties required to eliminate null warning

--- src/Chirp.CSVDBService/Models/CreateCheepRequestModel.cs
+++ src/Chirp.CSVDBService/Models/CreateCheepRequestModel.cs
@@ -2,6 +2,6 @@ namespace Chirp.CSVDBService.Models;
 
 public class CreateCheepRequestModel
 {
-    public string Author { get; set; }
-    public string Message { get; set; }
+    public required string Author { get; set; }
+    public required string Message { get; set; }
 }
