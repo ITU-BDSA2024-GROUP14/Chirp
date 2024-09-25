c89132e5815329346faac6c56a35d2e44d9292de
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-09-25 16:04:41 +0200

    Add new Core project for db-related stuff

---
+++ src/Chirp.Core/DBFacade.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Chirp.Core;
+
+public class DBFacade
+{
+    
+    public DBFacade(IConfigurationRoot configuration)
+    {
+        
+    }
+    
+    public void ExecuteQuery(string query)
+    {
+        
+    }
+}
