ca95d3eb409869c1481e630fd0f0c0b085b7d590
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-01 13:52:48 +0200

    Add SeedTestData method, using data/dump.sql

--- test/TestHelpers/DbFacadeFixture.cs
+++ test/TestHelpers/DbFacadeFixture.cs
@@ -1,4 +1,6 @@
 using Chirp.Core;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.FileProviders;
 
 namespace TestHelpers;
 
@@ -11,5 +13,27 @@ public class DbFacadeFixture
     {
         Db = new TestDatabase();
         DbFacade = new DBFacade(Db);
+    }
+
+    public void Reset()
+    {
+        Db.Reset();
+    }
+
+    public void SeedTestData()
+    {
+        var embeddedProvider = new EmbeddedFileProvider(typeof(IDatabase).Assembly);
+        using var reader = embeddedProvider.GetFileInfo("data/dump.sql").CreateReadStream();
+        using var sr = new StreamReader(reader);
+
+        var query = sr.ReadToEnd();
+
+        using var connection = new SqliteConnection(Db.ConnectionString);
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = query;
+
+        command.ExecuteNonQuery();
     }
 }
