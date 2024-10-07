84c5bfb2614fac337a2b121f4ea3e592a7f3639a
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-07 08:36:03 +0200

    Add CheepRepository

--- src/Chirp.Core/CheepRepository.cs
+++ src/Chirp.Core/CheepRepository.cs
@@ -1,5 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Chirp.Core;
 
-public class CheepRepository
+public interface ICheepRepository
 {
+    public IEnumerable<CheepRecord> GetCheeps(int page, string authorUsername);
+}
+
+public class CheepRepository : ICheepRepository
+{
+    private ChirpDBContext _dbcontext;
+
+    public CheepRepository(ChirpDBContext context)
+    {
+        _dbcontext = context;
+    }
+
+    public IEnumerable<CheepRecord> GetCheeps(int page, string? authorUsername = null)
+    {
+        var query = from cheep in _dbcontext.Cheeps
+            select cheep;
+        if (authorUsername != null)
+        {
+            query = from cheep in query
+                where cheep.Author.Name == authorUsername
+                select cheep;
+        }
+
+        return query.ToListAsync();
+    }
 }
