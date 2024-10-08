c9d8d6694cc61e0d030349988e7e2721e064c743
Esser <jones@itu.dk>
2024-10-08 13:30:06 +0200

    Add required attribute to fields in datamodel
    To supress warnings

--- src/Chirp.Core/DataModel/Cheep.cs
+++ src/Chirp.Core/DataModel/Cheep.cs
@@ -4,7 +4,7 @@ public class Cheep
 {
     public int CheepId { get; set; }
     public int AuthorId { get; set; }
-    public Author Author { get; set; }
-    public string Text { get; set; }
-    public DateTime TimeStamp { get; set; }
+    public required Author Author { get; set; }
+    public required string Text { get; set; }
+    public required DateTime TimeStamp { get; set; }
 }
