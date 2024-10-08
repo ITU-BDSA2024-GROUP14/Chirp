c175871940c3ee114844a54ea9b79db49d338098
Esser <jones@itu.dk>
2024-10-08 13:41:00 +0200

    Add default value for timeline cheeps

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -7,7 +7,7 @@ namespace Chirp.Razor.Pages;
 public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
-    public List<CheepDTO> Cheeps { get; set; }
+    public List<CheepDTO> Cheeps { get; set; } = [];
 
     public PublicModel(ICheepService service)
     {
