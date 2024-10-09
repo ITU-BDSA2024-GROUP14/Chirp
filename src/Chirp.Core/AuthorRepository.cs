41daa508ef0e193e095af6c00cc2b6cbf65ba48a
osta <osta@itu.dk>
2024-10-09 15:24:38 +0200

    Added possibility to get author by email
    Co-authored-by: Daniel <dtha@itu.dk>

--- src/Chirp.Core/AuthorRepository.cs
+++ src/Chirp.Core/AuthorRepository.cs
@@ -4,7 +4,7 @@ namespace Chirp.Core;
 
 public interface IAuthorRepository
 {
-    public Author GetAuthor(string authorName);
+    public Author GetAuthor(string? authorName = null, string? authorEmail = null);
 }
 
 public class AuthorRepository : IAuthorRepository
@@ -20,11 +20,24 @@ public class AuthorRepository : IAuthorRepository
     /// Get an author from the database
     /// </summary>
     /// <param name="authorName">The name of the author</param>
+    /// <param name="authorEmail">The email of the author</param>
     /// <returns>The requested author</returns>
-    public Author GetAuthor(string authorName)
+    public Author GetAuthor(string? authorName = null, string? authorEmail = null)
     {
+        if (authorName == null && authorEmail == null)
+        {
+            throw new ArgumentException("Please supply at least one argument that isnt null");
+        }
         var query = _dbcontext.Authors.AsQueryable();
-        query = query.Where(a => a.Name == authorName);
+        if (authorName != null)
+        {
+            query = query.Where(a => a.Name == authorName);
+        }
+
+        if (authorEmail != null)
+        {
+            query = query.Where(author => author.Email == authorEmail);
+        }
         return query.ToList().First();
     }
 }
