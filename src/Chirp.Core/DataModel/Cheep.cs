a67bcaf3d27dda5c2d6b9ab7d7db868d4ad56aa0
Esser <jones@itu.dk>
2024-10-20 20:21:06 +0200

    Limit cheep length

--- src/Chirp.Core/DataModel/Cheep.cs
+++ src/Chirp.Core/DataModel/Cheep.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Core.DataModel;
+
 /// <summary>
 /// Represents a Cheep, a short message posted by an Author.
 /// </summary>
@@ -7,6 +10,6 @@ public class Cheep
     public int CheepId { get; set; }
     public int AuthorId { get; set; }
     public required Author Author { get; set; }
-    public required string Text { get; set; }
+    [MaxLength(160)] public required string Text { get; set; }
     public required DateTime TimeStamp { get; set; }
 }
