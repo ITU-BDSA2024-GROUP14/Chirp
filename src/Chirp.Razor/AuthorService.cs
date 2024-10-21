152ce51840b975a53d5f0525d593c4288af292e5
osta <osta@itu.dk>
2024-10-21 08:21:56 +0200

    Created author service
    Co-authored-by: Daniel <dtha@itu.dk>

---
+++ src/Chirp.Razor/AuthorService.cs
@@ -0,0 +1,25 @@
+using Chirp.Core;
+using Chirp.Core.DataTransferObjects;
+
+namespace Chirp.Razor;
+
+public interface IAuthorService
+{
+    public AuthorDTO GetAuthor(string? authorName = null, string? authorEmail = null);
+}
+
+public class AuthorService : IAuthorService
+{
+    private IAuthorRepository _database;
+
+    public AuthorService(IAuthorRepository db)
+    {
+        _database = db;
+    }
+
+    public AuthorDTO GetAuthor(string? authorName = null, string? authorEmail = null)
+    {
+        var author = _database.GetAuthor(authorName: authorName, authorEmail: authorEmail);
+        var dto = new AuthorDTO {Name = author.Name, Email = author.Email};
+    }
+}
