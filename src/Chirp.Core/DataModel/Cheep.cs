f436c2bfe08c90e7ce59855ca6ecb0ceacc69fff
Esser <jones@itu.dk>
2024-10-21 08:45:09 +0200

    Undo changes for maxlength

--- src/Chirp.Core/DataModel/Cheep.cs
+++ src/Chirp.Core/DataModel/Cheep.cs
@@ -10,6 +10,6 @@ public class Cheep
     public int CheepId { get; set; }
     public int AuthorId { get; set; }
     public required Author Author { get; set; }
-    [MaxLength(160)] public required string Text { get; set; }
+    public required string Text { get; set; }
     public required DateTime TimeStamp { get; set; }
 }
