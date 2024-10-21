832823b1047b52bdb0978103d6f98e2b435799e9
Daniel <dtha@itu.dk>
2024-10-21 11:39:19 +0200

    Add database constraint for authors

--- src/Chirp.Core/DataModel/Author.cs
+++ src/Chirp.Core/DataModel/Author.cs
@@ -1,7 +1,12 @@
 namespace Chirp.Core.DataModel;
+
+using Microsoft.EntityFrameworkCore;
+
 /// <summary>
 /// Represents a user of the system.
 /// </summary>
+[Index(nameof(Name), IsUnique = true)]
+[Index(nameof(Email), IsUnique = true)]
 public class Author
 {
     public int AuthorId { get; set; }
