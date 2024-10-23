b656b67cf9bc6b534ce41b4f0ee42b7375451dc4
Daniel <dtha@itu.dk>
2024-10-23 15:38:33 +0200

    remove deprecated test + setup for future testing
    Co-authored-by: osta <osta@itu.dk>

--- test/Chirp.Infrastructure.Tests/AuthorRepositoryTests.cs
+++ test/Chirp.Infrastructure.Tests/AuthorRepositoryTests.cs
@@ -3,38 +3,17 @@ using Chirp.Core.DataModel;
 using Chirp.Infrastructure.Repositories;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using TestHelpers;
 
 namespace Chirp.Infrastructure.Tests;
 
-public class AuthorRepositoryTests
+public class AuthorRepositoryTests : IClassFixture<ChirpDbContextFixture>
 {
-    [Fact]
-    public void AuthorRepositoryGetAuthorByName()
+    private ChirpDbContextFixture _fixture;
+
+    public AuthorRepositoryTests(ChirpDbContextFixture fixture)
     {
-        var connection = new SqliteConnection("DataSource=:memory:");
-        connection.Open();
-        var options = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection).Options;
-        var author = new Author { Name = "Anna", AuthorId = 0, Email = "test@test.com" };
-        using (var context = new ChirpDBContext(options))
-        {
-            context.Database.EnsureCreated();
-            context.Authors.AddRange(
-                new Author {Name = "Bob", AuthorId = 69, Email = "john@doe.com"},
-                new Author { Name = "Chalie", AuthorId = 1337, Email = "this@isbad.com"},
-                author);
-            context.SaveChanges();
-        }
-
-        using (var context = new ChirpDBContext(options))
-        {
-            var service = new AuthorRepository(context);
-            var fromDB = service.GetAuthorByName(author.Name);
-
-            Assert.NotNull(fromDB);
-            Assert.Equal(author.Name, fromDB.Name);
-            Assert.Equal(author.AuthorId, fromDB.AuthorId);
-            Assert.Equal(author.Email, fromDB.Email);
-            Assert.Equal(author.Cheeps, fromDB.Cheeps);
-        }
+        _fixture = fixture;
+        _fixture.Reset();
     }
 }
