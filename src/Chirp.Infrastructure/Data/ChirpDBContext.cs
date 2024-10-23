350145d894a4f8c92953f4b1eb9bae705bfe37a3
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-23 13:50:50 +0200

    Use Fluent API instead of mapping attributes

--- src/Chirp.Infrastructure/Data/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/Data/ChirpDBContext.cs
@@ -2,6 +2,7 @@ using Chirp.Core.DataModel;
 using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Infrastructure.Data;
+
 /// <summary>
 /// Represents the database context for the Chirp application.
 /// </summary>
@@ -12,5 +13,13 @@ public class ChirpDBContext : DbContext
 
     public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>().HasIndex(a => a.Name).IsUnique();
+        modelBuilder.Entity<Author>().HasIndex(a => a.Email).IsUnique();
+
+        modelBuilder.Entity<Cheep>().Property(c => c.Text).HasMaxLength(Cheep.MaxLength);
     }
 }
