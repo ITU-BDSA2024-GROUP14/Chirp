09022c47d2ae18a7cf097291b375f2370d868d2d
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-29 14:14:29 +0100

    Set author name on register
    Co-authored-by: August Bugge <aubu@itu.dk>
    Co-authored-by: Jonas Esser <jones@itu.dk>

--- src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -111,6 +111,8 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                
+                user.Name = Input.UserName;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
