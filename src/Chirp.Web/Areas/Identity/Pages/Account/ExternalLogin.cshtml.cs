c36d823018c00899aa89f73d08a63ba148784e9d
osta <osta@itu.dk>
2024-10-30 11:46:22 +0100

    Added email to the created user on external login

--- src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -164,6 +164,7 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
                 user.Name = Input.DisplayName ?? throw new MissingFieldException();
+                user.Email = Input.Email ?? throw new MissingFieldException();
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
 
