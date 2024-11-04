765f3f39821d301a174bcd7633ad2e01f9e6875a
Daniel <dtha@itu.dk>
2024-11-04 09:43:50 +0100

    add create cheep from public timeline
    Co-authored-by: osta <osta@itu.dk>

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -8,6 +8,7 @@ namespace Chirp.Web.Pages;
 public class PublicModel : PageModel
 {
     private readonly IChirpService _service;
+    [BindProperty] public string Message { get; set; }
     public List<CheepDTO> Cheeps { get; set; } = [];
 
     public PublicModel(IChirpService service)
@@ -19,5 +20,23 @@ public class PublicModel : PageModel
     {
         Cheeps = _service.GetCheeps(page);
         return Page();
+    }
+
+    public ActionResult OnPost()
+    {
+        var authorName = User.Identity?.Name;
+        if (authorName == null)
+        {
+            return RedirectToPage("./PublicTimeline");
+        }
+
+        var author = _service.GetAuthorByName(authorName);
+        if (author == null)
+        {
+            return RedirectToPage("./PublicTimeline");
+        }
+
+        _service.CreateCheep(author.Name, author.Email, Message, DateTime.Now);
+        return RedirectToPage("./UserTimeline", new { authorName });
     }
 }
