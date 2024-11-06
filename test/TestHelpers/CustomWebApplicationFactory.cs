63dd758b2845e0c5e1c429d490fdb0eb9337dc8b
August Bugge <aubu@itu.dk>
2024-11-06 13:43:49 +0100

    Moved CustomWebApplicationFactory to TestHelpers
    So I can use it for endtoendtests

--- test/TestHelpers/CustomWebApplicationFactory.cs
+++ test/TestHelpers/CustomWebApplicationFactory.cs
@@ -6,7 +6,7 @@ using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace IntegrationTests;
+namespace TestHelpers;
 
 //Class based on code from .net documentation https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0#customize-webapplicationfactory
 public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
