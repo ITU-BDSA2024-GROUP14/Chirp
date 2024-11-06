3098d51dbe9f200e397822559979c4fc0b0c55e1
August Bugge <aubu@itu.dk>
2024-11-06 15:39:24 +0100

    An attempt to solve playwright problems

--- test/EndToEndTests/EndToEndTest.cs
+++ test/EndToEndTests/EndToEndTest.cs
@@ -8,10 +8,20 @@ using Microsoft.Playwright;
 
 namespace EndToEndTests;
 
-public class EndToEndTest
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class EndToEndTest : PageTest
 {
-    private readonly WebApplicationFactory<Program> _fixture;
-    private readonly HttpClient _client;
+    private WebApplicationFactory<Program> _fixture;
+
+    private HttpClient _client;
+
+
+    [OneTimeTearDown]
+    public void Cleanup()
+    {
+        _client.Dispose();
+    }
 
     public EndToEndTest(CustomWebApplicationFactory<Program> fixture)
     {
@@ -20,25 +30,20 @@ public class EndToEndTest
             new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
     }
 
-    [Parallelizable(ParallelScope.Self)]
-    [TestFixture]
-    public class Tests : PageTest
+    [Test]
+    public async Task TestLoginViaUserName()
     {
-        [Test]
-        public async Task TestLoginViaUserName()
-        {
-            await Page.GotoAsync("http://localhost:5273/");
-            await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
-            await Page.GetByPlaceholder("name@example.com").ClickAsync();
-            await Page.GetByPlaceholder("name@example.com").FillAsync("ropf@itu.dk");
-            await Page.GetByPlaceholder("password").ClickAsync();
-            await Page.GetByPlaceholder("password").ClickAsync();
-            await Page.GetByPlaceholder("password").FillAsync("LetM31n!");
-            await Page.GetByPlaceholder("password").PressAsync("Enter");
-            await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-            await Page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
-            await Page.GetByRole(AriaRole.Link, new() { Name = "logout [ropf@itu.dk]" }).ClickAsync();
-            await Page.GetByRole(AriaRole.Button, new() { Name = "Click here to Logout" }).ClickAsync();
-        }
+        await Page.GotoAsync("http://localhost:5273/");
+        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("ropf@itu.dk");
+        await Page.GetByPlaceholder("password").ClickAsync();
+        await Page.GetByPlaceholder("password").ClickAsync();
+        await Page.GetByPlaceholder("password").FillAsync("LetM31n!");
+        await Page.GetByPlaceholder("password").PressAsync("Enter");
+        await Page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Log in" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "my timeline" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "logout [ropf@itu.dk]" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Click here to Logout" }).ClickAsync();
     }
 }
--- test/TestHelpers/CustomWebApplicationFactory.cs
+++ test/TestHelpers/CustomWebApplicationFactory.cs
@@ -5,12 +5,30 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace TestHelpers;
 
 //Class based on code from .net documentation https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0#customize-webapplicationfactory
 public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
 {
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        // Create the host that is actually used by the
+        // TestServer (In Memory).
+        var testHost = base.CreateHost(builder);
+
+        // configure and start the actual host using Kestrel.
+        builder.ConfigureWebHost(webHostBuilder => webHostBuilder.UseKestrel());
+
+        var host = builder.Build();
+        host.Start();
+        // In order to cleanup and properly dispose HTTP server
+        // resources we return a composite host object that is
+        // actually just a way to intercept the StopAsync and Dispose
+        // call and relay to our HTTP host.
+        return new CompositeHost(testHost, host);
+    }
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
