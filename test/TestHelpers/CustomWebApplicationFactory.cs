f510663b3d89aab872d5e3ae963ec35fd33a8c48
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-11-11 11:04:20 +0100

    Re-add old CustomWebApplicationFactory and make the old WebpageTests use that

--- test/TestHelpers/CustomWebApplicationFactory.cs
+++ test/TestHelpers/CustomWebApplicationFactory.cs
@@ -1,80 +1,16 @@
 using System.Data.Common;
 using Chirp.Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace TestHelpers;
 
-/// <summary>
-/// This class is based on code from .NET docs: https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0#customize-webapplicationfactory
-/// and the following blog post: https://danieldonbavand.com/2022/06/13/using-playwright-with-the-webapplicationfactory-to-test-a-blazor-application/
-/// </summary>
-/// <typeparam name="TProgram">The startup class of the ASP.NET application to host</typeparam>
+//Class based on code from .net documentation https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0#customize-webapplicationfactory
 public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
 {
-    private IHost? _host;
-
-    public string ServerAddress
-    {
-        get
-        {
-            EnsureServer();
-            return ClientOptions.BaseAddress.ToString();
-        }
-    }
-
-    protected override IHost CreateHost(IHostBuilder builder)
-    {
-        // Create the host for TestServer now before we
-        // modify the builder to use Kestrel instead.
-        var testHost = builder.Build();
-
-        // Modify the host builder to use Kestrel instead
-        // of TestServer so we can listen on a real address.
-        builder.ConfigureWebHost(webHostBuilder => webHostBuilder.UseKestrel());
-
-        // Create and start the Kestrel server before the test server,
-        // otherwise due to the way the deferred host builder works
-        // for minimal hosting, the server will not get "initialized
-        // enough" for the address it is listening on to be available.
-        // See https://github.com/dotnet/aspnetcore/issues/33846.
-        _host = builder.Build();
-        _host.Start();
-
-        // Extract the selected dynamic port out of the Kestrel server
-        // and assign it onto the client options for convenience so it
-        // "just works" as otherwise it'll be the default http://localhost
-        // URL, which won't route to the Kestrel-hosted HTTP server.
-        var server = _host.Services.GetRequiredService<IServer>();
-        var addresses = server.Features.Get<IServerAddressesFeature>();
-
-        ClientOptions.BaseAddress = addresses!.Addresses
-            .Select(x => new Uri(x))
-            .Last();
-
-        // Return the host that uses TestServer, rather than the real one.
-        // Otherwise the internals will complain about the host's server
-        // not being an instance of the concrete type TestServer.
-        // See https://github.com/dotnet/aspnetcore/pull/34702.
-        testHost.Start();
-        return testHost;
-    }
-
-    private void EnsureServer()
-    {
-        if (_host is null)
-        {
-            // This forces WebApplicationFactory to bootstrap the server
-            using var _ = CreateDefaultClient();
-        }
-    }
-
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
@@ -105,10 +41,5 @@ public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProg
         });
 
         builder.UseEnvironment("Development");
-    }
-    
-    protected override void Dispose(bool disposing)
-    {
-        _host?.Dispose();
     }
 }
