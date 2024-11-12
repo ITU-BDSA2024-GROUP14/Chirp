dee903295c57e55b854b8bf365e7c07e921b5931
Esser <jones@itu.dk>
2024-11-12 13:02:53 +0100

    WIP test

---
+++ test/EndToEndTests/PlaywrightTest.cs
@@ -0,0 +1,18 @@
+﻿namespace EndToEndTests;
+
+using Microsoft.Playwright.NUnit;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    [Test]
+    public async Task MyTest()
+    {
+        await Page.GotoAsync("https://demo.playwright.dev/todomvc/#/");
+        await Page.GetByPlaceholder("What needs to be done?").ClickAsync();
+        await Page.GetByPlaceholder("What needs to be done?").FillAsync("I write!");
+        await Page.GetByPlaceholder("What needs to be done?").PressAsync("Enter");
+        await Expect(Page.GetByTestId("todo-title")).ToContainTextAsync("I write!");
+    }
+}
