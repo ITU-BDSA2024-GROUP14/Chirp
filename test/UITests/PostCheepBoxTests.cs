2c2cd92b93e7f8615d582068daaea5ab458162f1
Esser <jones@itu.dk>
2024-11-12 15:21:44 +0100

    Test for limited cheep length

--- test/UITests/PostCheepBoxTests.cs
+++ test/UITests/PostCheepBoxTests.cs
@@ -50,4 +50,26 @@ public class Tests : SelfHostedPageTest<Program>
         await Page.GotoAsync(serverAddress);
         await Expect(Page.GetByText("What's on your mind ropf@itu.dk? Share")).Not.ToBeVisibleAsync();
     }
+    
+    [Test]
+    public async Task LongCheepLengthReduced()
+    {
+        // Arrange
+        var serverAddress = GetServerAddress();
+        
+        //Act
+        await Page.GotoAsync(serverAddress);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("ropf@itu.dk");
+        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByPlaceholder("password").FillAsync("LetM31n!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Page.Locator("#Message").ClickAsync();
+        await Page.Locator("#Message").FillAsync("I wish to write the longest cheep anyone has ever seen. Oh, I hope that everyone who sees this cheep, in its full length will be filled with joy at my wonderful long Cheep");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+        await Expect(Page.Locator("#messagelist")).ToContainTextAsync("Helge I wish to write the longest cheep anyone has ever seen. Oh, I hope that everyone who sees this cheep, in its full length will be filled with joy at my wonderful");
+        await Expect(Page.Locator("#messagelist")).Not.ToContainTextAsync("Helge I wish to write the longest cheep anyone has ever seen. Oh, I hope that everyone who sees this cheep, in its full length will be filled with joy at my wonderful long Cheep");
+    }
+    
 }
