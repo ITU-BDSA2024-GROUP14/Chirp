fb2cec3b4f91ac80e3ffc35d8980ec94dc2a7091
osta <osta@itu.dk>
2024-11-12 14:30:30 +0100

    Made test that creates and cheeps from user
    This was done to ensure that the you can create a user, then login, then
    cheep and logout again

--- test/EndToEndTests/EndToEndTest.cs
+++ test/EndToEndTests/EndToEndTest.cs
@@ -8,12 +8,18 @@ namespace EndToEndTests;
 [TestFixture]
 public class EndToEndTest : SelfHostedPageTest<Program>
 {
+    private string serverAddress;
+
+    [SetUp]
+    public void Setup()
+    {
+        serverAddress = GetServerAddress();
+    }
 
     [Test]
     public async Task TestLoginViaUserName()
     {
-        // Arrange
-        var serverAddress = GetServerAddress();
+        
         
         //Act
         await Page.GotoAsync(serverAddress);
@@ -27,4 +33,45 @@ public class EndToEndTest : SelfHostedPageTest<Program>
         await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "logout [Helge]" }).ClickAsync();
         await Page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Click here to Logout" }).ClickAsync();
     }
+    
+    [Test]
+    public async Task TestCreateUserAndPostCheep()
+    {
+        await Page.GotoAsync(serverAddress);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("Test@example.dk");
+        await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
+        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Test1234!");
+        await Page.GetByLabel("Confirm Password").ClickAsync();
+        await Page.GetByLabel("Confirm Password").FillAsync("Test1234!");
+        await Page.GetByPlaceholder("JohnDoe").ClickAsync();
+        await Page.GetByPlaceholder("JohnDoe").FillAsync("Test user");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("Test@example.dk");
+        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByPlaceholder("password").FillAsync("Test1234!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Page.Locator("#Message").ClickAsync();
+        await Page.Locator("#Message").FillAsync("This is a test cheep");
+        await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "Share" })).ToBeVisibleAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Test user's Timeline" })).ToBeVisibleAsync();
+        await Expect(Page.GetByText("Test user This is a test")).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Paragraph)).ToContainTextAsync("Test user This is a test cheep —");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Public Timeline" })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "logout [Test user]" })).ToBeVisibleAsync();
+        await Expect(Page.Locator("li").Filter(new() { HasText = "Test user This is a test" })).ToBeVisibleAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "logout [Test user]" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "Click here to Logout" })).ToBeVisibleAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Click here to Logout" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Log out" })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "login" })).ToBeVisibleAsync();
+    }
+
+
 }
