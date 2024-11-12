8a712a1222dc3f079030ba444fcbe52782509428
Esser <jones@itu.dk>
2024-11-12 14:34:12 +0100

    Create PostCheepBoxTests.cs

---
+++ test/UITests/PostCheepBoxTests.cs
@@ -0,0 +1,53 @@
+using Microsoft.Playwright;
+using TestHelpers;
+
+namespace UITests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : SelfHostedPageTest<Program>
+{
+    [Test]
+    public async Task CheepBoxIsVisiblePublic()
+    {// Arrange
+        var serverAddress = GetServerAddress();
+        
+        //Act
+        await Page.GotoAsync(serverAddress);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("ropf@itu.dk");
+        await Page.GetByPlaceholder("password").ClickAsync();
+        await Page.GetByPlaceholder("password").FillAsync("LetM31n!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Expect(Page.GetByText("What's on your mind ropf@itu.dk? Share")).ToBeVisibleAsync();
+    }
+    
+    [Test]
+    public async Task CheepBoxIsVisiblePrivate()
+    {// Arrange
+        var serverAddress = GetServerAddress();
+        
+        //Act
+        await Page.GotoAsync(serverAddress);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("ropf@itu.dk");
+        await Page.GetByPlaceholder("password").ClickAsync();
+        await Page.GetByPlaceholder("password").FillAsync("LetM31n!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Page.GotoAsync(serverAddress + "Helge");
+        await Expect(Page.GetByText("What's on your mind ropf@itu.dk? Share")).ToBeVisibleAsync();
+    }
+    
+    [Test]
+    public async Task CheepBoxNotVisibleNotLoggedIn()
+    {
+        // Arrange
+        var serverAddress = GetServerAddress();
+        
+        //Act
+        await Page.GotoAsync(serverAddress);
+        await Expect(Page.GetByText("What's on your mind ropf@itu.dk? Share")).Not.ToBeVisibleAsync();
+    }
+}
