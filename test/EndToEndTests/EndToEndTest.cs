813ef78f505c22d0983bf1e4a2e127e73d844601
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-11-12 13:48:27 +0100

    Remove duplicate test

--- test/EndToEndTests/EndToEndTest.cs
+++ test/EndToEndTests/EndToEndTest.cs
@@ -27,23 +27,4 @@ public class EndToEndTest : SelfHostedPageTest<Program>
         await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "logout [ropf@itu.dk]" }).ClickAsync();
         await Page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Click here to Logout" }).ClickAsync();
     }
-    
-    [Test]
-    public async Task TestLoginViaUserName2()
-    {
-        // Arrange
-        var serverAddress = GetServerAddress();
-        
-        await Page.GotoAsync(serverAddress);
-        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "login" }).ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").FillAsync("ropf@itu.dk");
-        await Page.GetByPlaceholder("password").ClickAsync();
-        await Page.GetByPlaceholder("password").ClickAsync();
-        await Page.GetByPlaceholder("password").FillAsync("LetM31n!");
-        await Page.GetByPlaceholder("password").PressAsync("Enter");
-        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "my timeline" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "logout [ropf@itu.dk]" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Click here to Logout" }).ClickAsync();
-    }
 }
