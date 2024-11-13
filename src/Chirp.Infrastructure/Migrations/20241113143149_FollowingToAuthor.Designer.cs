c34e5b9824f772f1f477039099f376c2b97a1edf
osta <osta@itu.dk>
2024-11-13 15:37:11 +0100

    add migration for Following field in Author
    Co-authored-by: Daniel <dtha@itu.dk>

--- src/Chirp.Infrastructure/Migrations/20241113143149_FollowingToAuthor.Designer.cs
+++ src/Chirp.Infrastructure/Migrations/20241113143149_FollowingToAuthor.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Chirp.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chirp.Infrastructure.Migrations
 {
     [DbContext(typeof(ChirpDBContext))]
-    partial class ChirpDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241113143149_FollowingToAuthor")]
+    partial class FollowingToAuthor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
