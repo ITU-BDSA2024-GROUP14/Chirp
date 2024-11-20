088acec401168acec5263920e15ad7e32511dd1a
Esser <jones@itu.dk>
2024-11-20 16:00:24 +0100

    Add test for unfollow

--- test/UITests/PlaywrightTest.cs
+++ test/UITests/PlaywrightTest.cs
@@ -123,4 +123,23 @@ public class PlaywrightTest : SelfHostedPageTest
         await Page.GotoAsync(serverAddress +"Adrian");
         await Expect(Page.GetByText("Adrian Hej, velkommen til kurset.")).ToBeVisibleAsync();
     }
+    
+    [Test]
+    public async Task UnfollowTest()
+    {
+        await Page.GotoAsync(serverAddress);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("adho@itu.dk");
+        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByPlaceholder("password").FillAsync("M32Want_Access");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Page.Locator("li").Filter(new() { HasText = "Follow Jacqualine Gilcoine Starbuck now is what we hear the worst. — 01/08/23" }).GetByRole(AriaRole.Button).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
+        await Expect(Page.GetByText("Jacqualine Gilcoine Starbuck")).ToBeVisibleAsync();
+        await Expect(Page.Locator("#messagelist")).ToContainTextAsync("Jacqualine Gilcoine Starbuck now is what we hear the worst. — 01/08/23 13.17.39");
+        await Page.Locator("li").Filter(new() { HasText = "Unfollow Jacqualine Gilcoine Starbuck now is what we hear the worst. — 01/08/23" }).GetByRole(AriaRole.Button).ClickAsync();
+        await Expect(Page.GetByText("Jacqualine Gilcoine Starbuck")).Not.ToBeVisibleAsync();
+        await Expect(Page.Locator("#messagelist")).Not.ToContainTextAsync("Jacqualine Gilcoine Starbuck now is what we hear the worst. — 01/08/23 13.17.39");
+    }
 }
