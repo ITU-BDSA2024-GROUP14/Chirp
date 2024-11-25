ff223b77d3e1674ef71afbd50e4cdbcad45f9a22
Daniel <dtha@itu.dk>
2024-11-25 09:44:10 +0100

    refactor AuthorDTO to include have a contstructor

--- src/Chirp.Infrastructure/Data/DataTransferObjects/AuthorDTO.cs
+++ src/Chirp.Infrastructure/Data/DataTransferObjects/AuthorDTO.cs
@@ -4,9 +4,20 @@ namespace Chirp.Infrastructure.Data.DataTransferObjects;
 
 public class AuthorDTO
 {
-    public required string Name { get; set; }
-    public required string Email { get; set; }
-    public required List<CheepDTO> Cheeps { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public List<CheepDTO> Cheeps { get; set; }
 
-    public required List<AuthorDTO> Following { get; set; }
+    public List<string> Following { get; set; }
+
+    public AuthorDTO(Author author)
+    {
+        Name = author.Beak;
+        Email = author.Email;
+        if (author.Cheeps != null)
+        {
+            Cheeps = (List<CheepDTO>)author.Cheeps.Select(cheep => new CheepDTO(cheep));
+        }
+        Following = (List<string>)author.Following.Select(a => a.Beak);
+    }
 }
