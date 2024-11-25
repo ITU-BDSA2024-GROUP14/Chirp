bc282d72cb7b9eb8ce46b314f85484b7e33135fd
Daniel <dtha@itu.dk>
2024-11-25 09:27:21 +0100

    change in AuthorDTO to include cheeps and follow
    Co-authored-by: osta <osta@itu.dk>

--- src/Chirp.Infrastructure/Data/DataTransferObjects/AuthorDTO.cs
+++ src/Chirp.Infrastructure/Data/DataTransferObjects/AuthorDTO.cs
@@ -1,7 +1,12 @@
+using Chirp.Core.DataModel;
+
 namespace Chirp.Infrastructure.Data.DataTransferObjects;
 
 public class AuthorDTO
 {
     public required string Name { get; set; }
     public required string Email { get; set; }
+    public required List<CheepDTO> Cheeps { get; set; }
+
+    public required List<AuthorDTO> Following { get; set; }
 }
