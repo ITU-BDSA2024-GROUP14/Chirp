1ff8d4fa708ef2d733b6beaf8a0b960b78d4190c
Esser <jones@itu.dk>
2024-11-25 11:13:09 +0100

    Primitive UnfollowUser test
    Co-authored-by: Jonathan Antoine Villeret <jonv@itu.dk>

--- test/Chirp.Infrastructure.Tests/ChirpServiceTests.cs
+++ test/Chirp.Infrastructure.Tests/ChirpServiceTests.cs
@@ -301,4 +301,28 @@ public class ChirpServiceTests : IClassFixture<ChirpDbContextFixture>
         Assert.Contains(author1.Following, author => author==author2);
     }
     
+    [Fact]
+    public void UnFollowUser()
+    {
+        //Arrange
+        _fixture.SeedDatabase();
+        using var context = _fixture.CreateContext();
+        context.Database.EnsureCreated();
+        var cheeprepo = new CheepRepository(context);
+        var authorrepo = new AuthorRepository(context);
+        var service = new ChirpService(cheeprepo, authorrepo);
+        service.FollowUser("Roger Histand", "Luanna Muro");
+        var author1 = authorrepo.GetAuthorByName("Roger Histand");
+        var author2 = authorrepo.GetAuthorByName("Luanna Muro");
+        Assert.NotNull(author1);
+        Assert.NotNull(author2);
+        Assert.Contains(author1.Following, author => author==author2);
+        
+        //Act
+        service.UnFollowUser("Roger Histand", "Luanna Muro");
+        
+        //Assert
+        Assert.DoesNotContain(author1.Following, author => author==author2);
+    }
+    
 }
