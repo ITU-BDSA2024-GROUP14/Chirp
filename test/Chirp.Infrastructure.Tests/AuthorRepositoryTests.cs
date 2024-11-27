e067f90bb5d118d6bd106fb5aaf13e1f88b64983
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-11-27 13:03:35 +0100

    Remove duplicate GetAuthorByName test

--- test/Chirp.Infrastructure.Tests/AuthorRepositoryTests.cs
+++ test/Chirp.Infrastructure.Tests/AuthorRepositoryTests.cs
@@ -28,8 +28,10 @@ public class AuthorRepositoryTests : IClassFixture<ChirpDbContextFixture>
         context.Database.EnsureCreated();
         var authorrepo = new AuthorRepository(context);
         var name = "Helge";
+        
         //Act
         var author = authorrepo.GetAuthorByName(name);
+        
         //Assert
         Assert.NotNull(author);
         Assert.Equal(name, author.Beak);
@@ -50,36 +52,7 @@ public class AuthorRepositoryTests : IClassFixture<ChirpDbContextFixture>
         Assert.NotNull(author);
         Assert.Equal(name, author.Beak);
     }
-
-    [Fact]
-    public void AuthorRepositoryGetAuthorByName()
-    {
-        var connection = new SqliteConnection("DataSource=:memory:");
-        connection.Open();
-        var options = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection).Options;
-        var author = new Author { Beak = "Anna", Email = "test@test.com" };
-        using (var context = new ChirpDBContext(options))
-        {
-            context.Database.EnsureCreated();
-            context.Authors.AddRange(
-                new Author { Beak = "Bob", Email = "john@doe.com" },
-                new Author { Beak = "Chalie", Email = "this@isbad.com" },
-                author);
-            context.SaveChanges();
-        }
-
-        using (var context = new ChirpDBContext(options))
-        {
-            var service = new AuthorRepository(context);
-            var fromDB = service.GetAuthorByName(author.Beak);
-
-            Assert.NotNull(fromDB);
-            Assert.Equal(author.Beak, fromDB.Beak);
-            Assert.Equal(author.Email, fromDB.Email);
-            Assert.Equal(author.Cheeps, fromDB.Cheeps);
-        }
-    }
-
+    
     [Fact]
     public void UserFollow_User_Succeeds()
     {
