4291931c4e920d6d6de13facba6ab690ab254c57
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-12-17 14:02:13 +0100

    Make s_webApplicationFactory not static

--- test/TestHelpers/SelfHostedPageTest.cs
+++ test/TestHelpers/SelfHostedPageTest.cs
@@ -1,12 +1,11 @@
 using Microsoft.Playwright.NUnit;
-using NUnit.Framework;
 
 namespace TestHelpers;
 
 public abstract class SelfHostedPageTest : PageTest
 {
-    private static PlaywrightWebApplicationFactory<Program>? s_webApplicationFactory;
-    protected static IServiceProvider ServiceProvider => s_webApplicationFactory!.Services;
+    private PlaywrightWebApplicationFactory<Program>? s_webApplicationFactory;
+    protected IServiceProvider ServiceProvider => s_webApplicationFactory!.Services;
 
     protected SelfHostedPageTest()
     {
