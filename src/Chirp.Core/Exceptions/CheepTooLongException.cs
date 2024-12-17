5f644729b99fdafc4b0679f7ff08559050383a75
Daniel <dtha@itu.dk>
2024-12-17 10:53:32 +0100

    document exceptions

--- src/Chirp.Core/Exceptions/CheepTooLongException.cs
+++ src/Chirp.Core/Exceptions/CheepTooLongException.cs
@@ -1,7 +1,9 @@
 using Chirp.Core.DataModel;
 
 namespace Chirp.Core.Exceptions;
-
+/// <summary>
+/// This exception is thrown whenever a cheep that contains too many characters is tried to saved in the database.
+/// </summary>
 public class CheepTooLongException : Exception
 {
     private Cheep cheep;
