5246c9bc1d136b5b6077a37cd5213eecca35b11b
osta <osta@itu.dk>
2024-12-17 14:10:35 +0100

    Created documentation for the viewcomponents

--- src/Chirp.Web/ViewComponents/CheepBoxViewComponent.cs
+++ src/Chirp.Web/ViewComponents/CheepBoxViewComponent.cs
@@ -3,8 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Chirp.Web.ViewComponents;
 
+/// <summary>
+/// ViewComponent containing the cheepbox component
+/// </summary>
 public class CheepBoxViewComponent : ViewComponent
 {
+    /// <summary>
+    /// Returns the cheepbox, that allows for sending cheeps
+    /// </summary>
+    /// <param name="model">The model to connect the cheepbox to</param>
+    /// <returns>IViewComponentResult containing cheepbox</returns>
     public IViewComponentResult Invoke(TimelineModel model)
     {
         return View("CheepBox");
