83cf02700a69f241385699ef0568889f53782470
August Bugge <aubu@itu.dk>
2024-12-18 10:23:16 +0100

    Changed CheepTooLongException summery

--- src/Chirp.Core/Exceptions/CheepTooLongException.cs
+++ src/Chirp.Core/Exceptions/CheepTooLongException.cs
@@ -2,7 +2,7 @@ using Chirp.Core.DataModel;
 
 namespace Chirp.Core.Exceptions;
 /// <summary>
-/// This exception is thrown whenever a cheep that contains too many characters is tried to be saved in the database.
+/// This exception is thrown whenever there is an attempt to save a cheep that contains too many characters in the database.
 /// </summary>
 public class CheepTooLongException : Exception
 {
